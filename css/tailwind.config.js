20fe8719183dca58828938dd123298bb399adff8
Michael <michal_czekaj@yahoo.com>
2026-07-30 21:57:22 +0200

    POPRAWA RESPONSYWNOŚCI

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['../**/*.html'],
+  content: ['../*.html', '../**/*.html'],
   theme: {
     extend: {
       colors: {ink:'#3A3341',deep:'#3E3852',teal:'#A0525E',tealsoft:'#F7E9E7',sand:'#FAF5EF',sanddark:'#EDDFD3'},
